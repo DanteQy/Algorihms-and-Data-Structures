4b4ad6cefe73d19a54952fd547696aef574b9a63
DanteQy <dante.qyshka@outlook.it>
2018-01-02 18:13:37 +0100

    Correction
    Corrected the format and commented the countingSort method

--- CountingSort/CountingSort.java
+++ CountingSort/CountingSort.java
@@ -14,12 +14,12 @@ import java.util.Random;
  */
 public class CountingSort {
  
-
     /**
      * @param args the command line arguments
      */
     //Time complexity Big Theta(n+k)
     static int[] countingSort(int in[]) {
+        
         int leng = in.length; 
         int max = getMax(in);
         //the size of the array is +1 from the max value since it start from 0
@@ -29,25 +29,27 @@ public class CountingSort {
         //max = k
         //leng = n
         
-        //O(k)
+        //O(k) - set count to zeros
         for(int i = 0; i < max; i++){
             count[i] = 0; 
         } 
         
-        System.out.println("-----------"); 
+        //increase the element on the in[j] position on the count array
         //O(n)
         for(int j = 0; j < leng; j++){
             count[in[j]] = count[in[j]]+1;   
         } 
-        
-        System.out.println("2 : "+Arrays.toString(count));
+         
+        //sum every element with the previous, starting from the 2nd element
         //O(k)
         for(int i = 1 ; i <= max; i++){ 
-            count[i] =count[i]+ count[i-1]; 
-            
+            count[i] =count[i]+ count[i-1];    
         }
-        
-        System.out.println("///////////");
+         
+        //Take the value from in[j]
+        //-to find the correct position in count[in[j]]
+        //-subtract 1 to this value since it out starts from 0
+        //Decrease the value taken from count
         //O(n)
         for(int j = 0; j < leng; j++){
             out[count[in[j]]-1] = in[j];
@@ -56,11 +58,12 @@ public class CountingSort {
          
         checkSort(in);
         return out;
-       
     }
 
      static void checkSort(int[] arr){
+         
         boolean test =true;
+        
         for(int i = 1; i <arr.length; i++ ){
             if(arr[i]<arr[i-1])
                 test = false;
@@ -73,6 +76,7 @@ public class CountingSort {
     public static void main(String args[]) {
         int leng = 15; 
         int arr[] = populateArr(leng);
+        
         System.out.println(Arrays.toString(countingSort(arr)));
     }
     
@@ -80,6 +84,7 @@ public class CountingSort {
     {
         int l = arr.length;
         int mx = arr[0];
+        
         for (int i = 1; i < l ; i++)
             if (arr[i] > mx)
                 mx = arr[i];
@@ -90,7 +95,8 @@ public class CountingSort {
 		
 	    int[] sort = new int[n];
 	    Random random = new Random();
-	    for (int i = 0; i < sort.length; i++) {
+	
+            for (int i = 0; i < sort.length; i++) {
 		    sort[i] = random.nextInt(10);
 	    }
 	
